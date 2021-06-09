9659aa5ecbef6e44f259fadeb0badd380da28d53
Douglas Krug <dkrug82@gmail.com>
2021-06-09 10:42:21 -0400

    converted notebook to python file and cleaned it up

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape():
+    #NASA Mars News
+    # Set up Browser
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    #Visit website
+    url = "https://redplanetscience.com/"
+    browser.visit(url) 
+    #Set up BeautifulSoup to find what is needed   
+    html = browser.html
+    soup = bs(html, "html.parser")
+    news_title = soup.find('div', class_='content_title').get_text()
+    print(news_title)
+    #scrape for paragraph text for news title
+    news_p = soup.find('div', class_='article_teaser_body').get_text()
+    print(news_p)
+    browser.quit()
+
+    # # JPL Mars Space Images - Featured Image
+    #set up browser
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    #visit website
+    url = "https://spaceimages-mars.com/"
+    browser.visit(url)
+    #Set up BeautifulSoup to find what is needed   
+    html = browser.html
+    soup = bs(html, "html.parser")
+    #parsed to find the image source
+    header_image = soup.find('img', class_='headerimage')['src']
+    print(header_image)
+    #added image source to original url to create the full image link
+    featured_image_url = url + header_image
+    print(featured_image_url)
+    browser.quit()
+
+    # # Mars Facts
+    #set up url
+    url = "https://galaxyfacts-mars.com/"
+
+    #Scrapped tables from URL 
+    tables = pd.read_html(url)
+    tables
+    #Created dataframe from tables list element
+    df = tables[0]
+    #Renamed columns
+    df.columns = ['Properties', 'Mars', 'Earth']
+    #dropped first row to eleminate unnecessary items
+    df = df.iloc[1:]
+    df = df.reset_index(drop=True)
+    df
+    #Converted dataframe to HTML Tabls
+    html_table = df.to_html()
+    html_table
+    #Stripped unwanted newlines 
+    stripped_html_table = html_table.replace('\n', '')
+
+    # # Mars Hemispheres    
+    #set up browser
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    #visit website
+    url = "https://marshemispheres.com/"
+    browser.visit(url)
+    #Set up BeautifulSoup to find what is needed   
+    html = browser.html
+    soup = bs(html, "html.parser")
+    #empty list to store links to get to pages to get title and image
+    links = []
+    #scrap page to get to parent div of each link
+    divs = soup.find_all('div', class_='description')
+    #looped through divs to find the full link
+    for div in divs:
+        description = div.find('a', class_='itemLink')['href']
+        #print(description)
+        full_link = url + description
+        #appended links list to add each full link
+        links.append(full_link)
+    print(links)
+    #empty list to hold dictionaries of each result
+    hemisphere_image_urls = []
+    #looped through each link in link list to find image and title
+    for link in links:
+        #used to visit each links page
+        browser.visit(link)
+        #reset BeautifulSoup to new browser
+        html = browser.html
+        soup = bs(html, "html.parser")
+        #parsed to find image url
+        img_url = soup.find_all('a', target='_blank')[3]['href']
+        #added img_url to the original url to create a full link to image
+        full_img = url + img_url
+        #parsed to find title
+        title = soup.find('h2', class_='title').text
+        #saved results to a dictionary
+        hem_info = {'title':title,
+                    'img':full_img}
+        #appended empty list with the dictionary
+        hemisphere_image_urls.append(hem_info)
+    
+    print(hemisphere_image_urls)
+    browser.quit()
+
+    #Store data in dictionary
+    mars_data = {
+        "news_title":news_title,
+        "news_p":news_p,
+        "featured_image":featured_image_url,
+        "facts_table":stripped_html_table,
+        "hemispheres":hemisphere_image_urls
+    }
+
+    return mars_data
+
+
